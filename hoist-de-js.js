630fb96e9d1403bb76e2376a9616348f28885c70
Kenny Julián Luque Ticona <kenny.luque.t@uni.pe>
2022-04-25 11:57:03 -0500

    hoist

---
+++ hoist-de-js.js
@@ -0,0 +1,126 @@
+console.log(hello);                                   
+var hello = 'world';                                 
+
+// var hello;
+// console.log(hello);                                   
+// var hello = 'world';                                 
+
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// var needle;
+// var needle = 'haystack';
+// function test() {
+//     var needle;
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+// test();
+
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// var brendan;
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+
+// var food;
+// var food = 'chicken';
+// console.log(food);
+// function eat() {
+//     var food;
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+// eat();
+
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+// var mean;
+// mean();
+// console.log(food);
+// var mean = function() {
+    // var food;
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// var genre;
+// function rewind() {
+    // var genre;
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+// var genre = "disco";
+// console.log(genre);
+// rewind();
+
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// function learn() {
+    // var dojo;
+//     dojo = "seattle";
+//     console.log(dojo);
+//     dojo = "burbank";
+//     console.log(dojo);
+// }
+// dojo = "san jose";
+// console.log(dojo);
+// console.log(dojo);
+// learn();
